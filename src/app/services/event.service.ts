68085cec288c22ee3c2b5d1647ac3ca482cc92ad
AgusXDeath <gonzacofreesc@gmail.com>
2024-10-03 11:52:30 -0300

    actualizo por las dudas

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -11,25 +11,49 @@ export class EventService {
 
   constructor(private http: HttpClient) { }
 
+
+  addEvento(eventoData: Evento): Observable<Evento[]> {
+    console.log(eventoData);
+    return this.http.post<Evento[]>(`${this.baseUrl}../core/Router.php`, eventoData);
+  }
+
   getEventos(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(`${this.baseUrl}/getEventos.php`);
+    return this.http.get<Evento[]>(`${this.baseUrl}../core/Router.php`);
   }
 
-  getEventoById(uid: number): Observable<Evento> {
-    return this.http.get<Evento>(`${this.baseUrl}/getEventoById.php?idEvento=${uid}`);
+  getEventoById(uid: number): Observable<Evento[]> {
+    return this.http.get<Evento[]>(`${this.baseUrl}../core/Router.php?action=get&id=${uid}`);
   }
 
-  addEvento(evento: Evento) {
-    return this.http.post(`${this.baseUrl}/addEvento.php`, evento);
+  deleteEvento(evento: Evento): Observable<Evento[]> {
+    console.log(evento);
+    return this.http.delete<Evento[]>(`${this.baseUrl}../core/Router.php?action=delete=${evento}`);
   }
 
-  deleteEvento(evento: Evento) {
-    return this.http.delete(`${this.baseUrl}/deleteEvento.php?idEvento=${evento.uid}`);
+  updateEvento(evento: Evento): Observable<Evento[]> {
+    console.log(evento);
+    return this.http.put<Evento[]>(`${this.baseUrl}../core/Router.php`, evento);
   }
 
-  updateEvento(evento: Evento): Observable<any> {
-    return this.http.put(`${this.baseUrl}/updateEvento.php`, evento);
-  }
+  // getEventos(): Observable<Evento[]> {
+  //   return this.http.get<Evento[]>(`${this.baseUrl}/getEventos.php`);
+  // }
+
+  // getEventoById(uid: number): Observable<Evento> {
+  //   return this.http.get<Evento>(`${this.baseUrl}/getEventoById.php?idEvento=${uid}`);
+  // }
+
+  // addEvento(evento: Evento) {
+  //   return this.http.post(`${this.baseUrl}/addEvento.php`, evento);
+  // }
+
+  // deleteEvento(evento: Evento) {
+  //   return this.http.delete(`${this.baseUrl}/deleteEvento.php?idEvento=${evento.uid}`);
+  // }
+
+  // updateEvento(evento: Evento): Observable<any> {
+  //   return this.http.put(`${this.baseUrl}/updateEvento.php`, evento);
+  // }
   
 }
 
