b461e39342b41b7495c49bed1c0d3be8825119c1
MayraCalderon05 <may2005_cc@hotmail.com>
2024-10-04 08:57:38 -0300

    Desde el servicio cambié el tipo de dato que van a recibir cada uno de los modulos de listar, editar, get y eliminar ya que saltaba un error en la compatibilidad de los datos

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -7,36 +7,36 @@ import { Observable } from 'rxjs'; // manejar las respuestas asincrónicas de la
   providedIn: 'root'
 })
 export class EventService {
-  private baseUrl = 'http://localhost/api-jueves/API-municipalidad/public/index.php'; // URL base para las peticiones HTTP, donde se harán a un backend en PHP alojado localmente.
+  private baseUrl = 'http://localhost/api/public/index.php'; // URL base para las peticiones HTTP, donde se harán a un backend en PHP alojado localmente.
 
   constructor(private http: HttpClient) { }
 
 
-  addEvento(eventoData: Evento): Observable<Evento[]> {
+  addEvento(eventoData: Evento): Observable<any> {
     console.log(eventoData);
-    return this.http.post<Evento[]>(`${this.baseUrl}../core/Router.php`, eventoData);
+    return this.http.post(`${this.baseUrl}../core/Router.php`, eventoData);
   }
 
-  getEventos(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(`${this.baseUrl}../core/Router.php`);
+  getEventos(): Observable<any> {
+    return this.http.get(`${this.baseUrl}../core/Router.php`);
   }
 
-  getEventoById(uid: number): Observable<Evento[]> {
-    return this.http.get<Evento[]>(`${this.baseUrl}../core/Router.php?action=get&id=${uid}`);
+  getEventoById(uid: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}../core/Router.php?action=get&id=${uid}`);
   }
 
-  deleteEvento(evento: Evento): Observable<Evento[]> {
+  deleteEvento(evento: Evento): Observable<any> {
     console.log(evento);
-    return this.http.delete<Evento[]>(`${this.baseUrl}../core/Router.php?action=delete=${evento}`);
+    return this.http.delete(`${this.baseUrl}../core/Router.php?action=delete=${evento}`);
   }
 
-  updateEvento(evento: Evento): Observable<Evento[]> {
+  updateEvento(evento: Evento): Observable<any> {
     console.log(evento);
-    return this.http.put<Evento[]>(`${this.baseUrl}../core/Router.php`, evento);
+    return this.http.put(`${this.baseUrl}../core/Router.php`, evento);
   }
 
-  // getEventos(): Observable<Evento[]> {
-  //   return this.http.get<Evento[]>(`${this.baseUrl}/getEventos.php`);
+  // getEventos(): Observable {
+  //   return this.http.get(`${this.baseUrl}/getEventos.php`);
   // }
 
   // getEventoById(uid: number): Observable<Evento> {
