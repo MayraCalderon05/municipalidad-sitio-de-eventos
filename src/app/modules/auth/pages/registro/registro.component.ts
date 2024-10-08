8034b00c8465c181c5d1afc6a6dde6d7d9c9a741
MayraCalderon05 <may2005_cc@hotmail.com>
2024-10-08 12:46:58 -0300

    validacion de los campos del formulario y está en proceso el enviar ese objeto para que lo guarde la base de datos

--- src/app/modules/auth/pages/registro/registro.component.ts
+++ src/app/modules/auth/pages/registro/registro.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
-import {FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { first } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
   //? variable para que muestre o no la contraseña del campo
   hide = true;
+  formularioRegistro: FormGroup;
 
-  //?validacion del email
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  //?en caso de que el email sea invalido
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'Debe de ingresar un email';
-    }
-
-    return this.email.hasError('email') ? 'El email ingresado no es válido' : '';
+  constructor(private fb: FormBuilder, private servicioAuth: AuthService, private router: Router){
+    //? validacion de los campos para que los necesarios no esten vacios
+    this.formularioRegistro = this.fb.group({
+      nombre: ['',[Validators.required]],
+      apellido: ['',[Validators.required]],
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required]],
+      telefono: [299,[Validators.minLength(10), Validators.pattern('^[0-9]+$')]] //* este tipo de validacion dice que no es requerido el campo pero tiene un minimo y solo acepta caracteres numericos
+    })
   }
+
+  onSubmit(){
+    if (this.formularioRegistro.valid) {
+      //this.servicioAuth.registrarUsuario()
+    }
+  }
+
+  
+
+  ngOnInit(){}
 
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import  { HttpClient } from '@angular/common/http';
 import { User } from 'src/app/models/user';
 
@@ -10,4 +10,14 @@ export class AuthService {
   private baseUrl = 'http://localhost/api/index.php'; // URL base para las peticiones HTTP, donde se harán a un backend en PHP alojado localmente.
 
   constructor(private http: HttpClient) { }
+
+  registrarUsuario(usuarioData: User): Observable<any>{
+    console.log(usuarioData);
+    return this.http.post(`${this.baseUrl}../core/RouterUsuario.php`, usuarioData)
+    .pipe(map(User =>{
+      return User;
+    }))
+  }
+
+
 }
