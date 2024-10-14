f9d7eb19da5dfe36fa1175759d3adf2718223bd9
ign4cio17 <ignaacioo.2024@gmail.com>
2024-10-14 14:41:39 -0300

    ya termine el prototipo le la vista de inicio de los eventos con ejemplos de como se veria con los cards dinamicos cargados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
 
 const routes: Routes = [
+  {path:'inicio',loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)},
   { path: 'eventos', loadChildren: () => import('./modules/evento/evento.module').then(m => m.EventoModule) },
   { path: '', redirectTo: '/eventos', pathMatch: 'full' },
   { path: "**", redirectTo: "/eventos" },
