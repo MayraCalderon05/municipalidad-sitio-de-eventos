d585248fa04887b3a46ebf0a8b09602cba0fc5e6
MayraCalderon05 <may2005_cc@hotmail.com>
2024-10-18 00:38:11 -0300

    cambie los componentes por modulos

--- src/app/modules/admin/pages/admin/admin.component.ts
+++ src/app/modules/admin/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//- importacion de los componentes que voy a usar directamente
 import { ListEventComponent } from 'src/app/modules/evento/components/list-event/list-event.component';
 import { UsuarioComponent } from '../../components/usuario/usuario.component';
 import { GruposComponent } from '../../components/grupos/grupos.component';
@@ -9,14 +10,14 @@ import { GruposComponent } from '../../components/grupos/grupos.component';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-  componenteActual: any = ListEventComponent;
-  
+  componenteActual: any = ListEventComponent; //! componente predeterminado
+  //? establezco las claves que si o si van a entrar como cadenas y las redirijo a cada componente
   componentes: Record<'eventos' | 'usuarios' | 'grupos', any> = {
     eventos: ListEventComponent,
     usuarios: UsuarioComponent,
     grupos: GruposComponent,
   };
-
+  //? en caso de que este alguna de las claves, actualiza el componete
   mostrarComponente(componente: 'eventos' | 'usuarios' | 'grupos') {
     this.componenteActual = this.componentes[componente];
   }
