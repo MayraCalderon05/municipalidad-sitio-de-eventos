107279238167db2c1134af959e9a0efc067016b9
MayraCalderon05 <may2005_cc@hotmail.com>
2024-10-18 00:22:13 -0300

    Creacion de los componentes donde se van a mostrar usuario y grupo de usuarios; configuacion de la sideNav de admin.module para que muestre los componentes

--- src/app/modules/admin/pages/admin/admin.component.ts
+++ src/app/modules/admin/pages/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ListEventComponent } from 'src/app/modules/evento/components/list-event/list-event.component';
+import { UsuarioComponent } from '../../components/usuario/usuario.component';
+import { GruposComponent } from '../../components/grupos/grupos.component';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  componenteActual: any = ListEventComponent;
+  
+  componentes: Record<'eventos' | 'usuarios' | 'grupos', any> = {
+    eventos: ListEventComponent,
+    usuarios: UsuarioComponent,
+    grupos: GruposComponent,
+  };
+
+  mostrarComponente(componente: 'eventos' | 'usuarios' | 'grupos') {
+    this.componenteActual = this.componentes[componente];
+  }
 
 }
